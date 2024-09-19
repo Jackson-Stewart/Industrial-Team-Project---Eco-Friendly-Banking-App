7978f8f842abba5b54534c5785b7384f0e0e457a
KyleTait01 <taitk58@gmail.com>
2024-09-19 11:56:53 +0100

    set up tailwind

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["pages/*.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    screens: {
+      'sm': '640px',      
+      'md': '768px',      
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1560px'
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif']
+      },
+      colors: {
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
