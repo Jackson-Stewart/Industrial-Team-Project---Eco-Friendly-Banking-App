f7e3fad6b5f45f15b021bee67928f412aab8e5e1
samtweedie <76115945+samtweedie@users.noreply.github.com>
2024-09-26 13:03:38 +0100

    added POST api call for send money page

--- Frontend/sendMoney.js
+++ Frontend/sendMoney.js
@@ -1,5 +1,43 @@
 const url = "https://efnn495zpi.execute-api.us-east-1.amazonaws.com";
 
+var getExtension = ""
+
+async function parseJSONObject(type) {
+    // Call API, get requested content and change into JSON object
+    let userObject = '';
+    return new Promise((resolve, reject) => {
+        const apiUrl = url + "/api/transaction?" + getExtension;
+        fetch(apiUrl, {
+            cache: 'no-cache',
+            method: type,
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                userObject = data[0];
+                resolve(userObject);
+            })
+    })
+}
+
+async function sendMoneyClick() {
+    var recipientInput = document.getElementById("recipient");
+    var amountInput = document.getElementById("amount");
+    var referenceInput = document.getElementById("reference");
+    
+    getExtension = "accountNumber=" + recipientInput;
+    const greenscoreObject = await parseJSONObject("GET"); //GREENSCORE
+    
+    getExtension = "accountNumberFrom=" + localStorage.get("accountNumber")
+                + "&accountNumberTo=" + recipientInput
+                + "&amount=" + amountInput
+                + "&calculatedGreenScore=" + greenscoreObject.greenscore
+                + "&reference=" + referenceInput;
+    const object = await parseJSONObject("POST");
+    if(object != null) {
+        launchDialog();
+    }
+}
+
 function tryAgain() {
     const wrongRecipient = document.getElementById('wrongRecipient');
     wrongRecipient.close();
@@ -8,20 +46,4 @@ function tryAgain() {
 function launchDialog() {
     const d = document.getElementById('wrongRecipient');
     wrongRecipient.showModal();
-}
-
-async function postTransaction(type) {      //api url needs changed
-    let userObject = '';
-    return new Promise((resolve, reject) => {
-        const apiUrl = url + "/api" + getExtension;
-        fetch(apiUrl, {
-            cache: 'no-cache',
-            method: 'POST',
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                userObject = data[0];
-                resolve(userObject);
-            })
-    })        
 }
