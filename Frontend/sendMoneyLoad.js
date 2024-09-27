52d42f21700a0c93319016919b861546bf3e29e7
quino82 <74664473+quino82@users.noreply.github.com>
2024-09-27 11:25:31 +0100

    Update sendMoneyLoad.js

--- Frontend/sendMoneyLoad.js
+++ Frontend/sendMoneyLoad.js
@@ -10,7 +10,7 @@ var userBalance = document.getElementById("userBalance");
 
 //Code from https://medium.com/@tejasshahade5/how-to-fetch-data-from-an-api-using-fetch-function-2147c7330a71#:~:text=The%20fetch()%20function%20sends,as%20its%20status%20and%20headers.
 try {
-    const userInfo = await fetchApiFunction(apiEndpoint);
+    const response = await fetchApiFunction();
 
     if(!response.ok) {
         throw new Error('Network response was not ok: ${response.status}');
