94524229855875deb66f220b54016f9c12b5083d
quino82 <74664473+quino82@users.noreply.github.com>
2024-10-03 11:34:08 +0100

    Update admin.js

--- Frontend/admin.js
+++ Frontend/admin.js
@@ -3,9 +3,18 @@ var discountAmount = document.getElementById("discountAmount");
 var costOfPoints = document.getElementById("costOfPoints");
 var submit = document.getElementById("submit");
 
+localStorage.setItem("newReward", false);
+
 submit.addEventListener('click', function () {
     localStorage.setItem("description", description.value);
 	localStorage.setItem("discountAmount", discountAmount.value);
 	localStorage.setItem("costOfPoints", costOfPoints.value);
+
+	//If the fields have been filled
+	if (description.value.length != 0 && discountAmount.value.length != 0 && costOfPoints.value.length != 0
+	{
+		localStorage.setItem("newReward", true);
+	}
+	
 	window.location.href = "login.html";
 });
